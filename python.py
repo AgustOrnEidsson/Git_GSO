930bb8baa96e4022d0f11427f176fb2348c33493
Ágúst Örn Eiðsson <agustei295@nemi.tskoli.is>
2017-01-24 11:22:42 +0000

    Opnar og skrifar 3 línur

--- python.py
+++ python.py
@@ -1,5 +1,10 @@
 #Git gagnasafna
 text_file=open("texti.txt", "w")
 
-text_file.write("Eru ekki allir í stuði eða???")
+text_file.write("Eru ekki allir i studi eda???")
 text_file.close()
+
+text_file=open("texti.txt", "a")
+text_file.write("\n" + "Eru ekki allir i studi eda???")
+text_file.write("\n" + "Eru ekki allir i studi eda???")
+text_file.write("\n" + "Eru ekki allir i studi eda???")
